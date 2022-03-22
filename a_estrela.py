460e16d3730e599c443bf85423c85e3019285abd
Felipe Jun <felipekumada@outlook.com>
2022-03-21 21:26:22 -0300

    b

--- a_estrela.py
+++ a_estrela.py
@@ -3,32 +3,35 @@ import heapq
 import numpy as np
 from map_contructor import *
 
-def a_estrela(estado_inicial, testar_objetivo, gerar_sucessores, heuristica, custo, stepEstado=False, stepSucessores=False):
+def a_estrela(estado_inicial, testar_objetivo, gerar_sucessores, heuristica, custo,M , N):
   fila = FilaPrioridade()
   fila.push(No(estado_inicial, None, None, 0.0, heuristica(estado_inicial),0.0))
+  saida = [0,[]]
+  custo_total = 0
 
   while not fila.esta_vazio():
     no_atual = fila.pop()
     estado_atual = no_atual.estado
-    # faz o teste objetivo conforme a função teste_objetivo
-    # para a execução se achou o objetivo
+    
+    print("\n")
+    print(np.reshape(estado_atual, (M, N)))
     if(testar_objetivo(estado_atual,len(estado_atual))):
-      return no_atual
-
-    print("\n")
-    print(np.reshape(estado_atual, (4, 4)))
-    print("\n")
+      return saida
     # verifico os nos filhos e os adiciono na fila
     # função sucessores define os estados seguintes e adiciona os nós seguintes
     estados_vertices_sucessores = gerar_sucessores(estado_atual)
     movimento = None
     for estados_vertices_sucessor in estados_vertices_sucessores:
+      no = No(estados_vertices_sucessor[0], no_atual, estados_vertices_sucessor[1],estados_vertices_sucessor[2], heuristica(estados_vertices_sucessor[0]))
       if movimento == None: 
-        movimento = No(estados_vertices_sucessores[0], estado_atual, estados_vertices_sucessores[1],estados_vertices_sucessores[3], heuristica(estado_inicial))
+        movimento = no
       else:
-        if estados_vertices_sucessor.total < movimento.total:
-
-    fila.push(No(estado_filho, no_atual, vertice, novo_custo, heuristica(estado_filho)))
+        if no.total < movimento.total:
+          movimento = no
+    custo_total += movimento.custo
+    saida[0] = custo_total
+    saida[1].append(movimento.vertice)
+    fila.push(movimento)
   return None
 
 class FilaPrioridade:
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from cmath import sqrt
 import random as rand
-import math
 from map_contructor import *
 import numpy as np
 from a_estrela import a_estrela
@@ -9,17 +9,12 @@ class Jogo:
     def testar_objetivo(self,map,array_size):
         return map[array_size - 1] == 0
 
-    # Função que gera os sucessores válidos 
-    # a partir de um estado válido
     def gerar_sucessores(self, estado):
         sucessores = []
 
-        # encontra a posição do _
         posicao = estado.index(0)
-        print(posicao)
-
         expansoes = [self._direita, self._esquerda, self._cima, self._baixo]
-        #rand.shuffle(expansoes)
+        rand.shuffle(expansoes)
         for expansao in expansoes:
             sucessor = expansao(posicao, estado)
             if sucessor is not None: sucessores.append(sucessor)
@@ -28,79 +23,82 @@ class Jogo:
 
     def _esquerda(self, posicao, estado_atual):
         esquerda = []
-        esquerda_values = []
         for i in range(len(map)):
-            #esquerda
             if i == 0 or i % N == 0:
                 esquerda.append(i)
-                esquerda_values.append(estado_atual[i])
         if posicao not in esquerda and 0 < estado_atual[posicao-1]:
-            # peça de baixo desce
             sucessor = list(estado_atual)
             sucessor[posicao] = -2
             sucessor[posicao - 1] = 0
-            return (tuple(sucessor), "⬅️",posicao,estado_atual[(posicao - 1)])
+            return (tuple(sucessor), "⬅️",estado_atual[(posicao - 1)])
 
     def _cima(self, posicao, estado_atual):
-        # movimento para cima
-        ## Não gera se estiver no topo
         cima = []
-        cima_values = []
         for i in range(len(map)):
-            #cima
             if 0 <= i < N:
                 cima.append(i)
-                cima_values.append(estado_atual[i])
         if posicao not in cima and 0 < estado_atual[posicao-N]:
-            # peça de baixo sobe
             sucesso = list(estado_atual)
             sucesso[posicao] = -2
             sucesso[posicao - N] = 0
-            return (tuple(sucesso), "⬆️",posicao,estado_atual[(posicao - N)])
+            return (tuple(sucesso), "⬆️",estado_atual[(posicao - N)])
 
     def _baixo(self, posicao, estado_atual):
-        # movimento para baixo
-        ## Não gera se estiver no fundo
         baixo = []
-        baixo_values = []
         for i in range(len(map)):
-            #baixo
             if len(map) % 2 == 0:
                 if i >= (M * N) - M:
                     baixo.append(i)
-                    baixo_values.append(estado_atual[i])
             else:
                 if i >= (M * N) - N:
                     baixo.append(i)
-                    baixo_values.append(estado_atual[i])
         if posicao not in baixo and 0 < estado_atual[posicao+M]:
-            # peça de baixo desce
             sucessor = list(estado_atual)
             sucessor[posicao] = -2
             sucessor[posicao + M] = 0
-            return (tuple(sucessor), "⬇️",posicao,estado_atual[(posicao + M)])
+            return (tuple(sucessor), "⬇️",estado_atual[(posicao + M)])
 
     def _direita(self, posicao, estado_atual):
-    # movimento para direita
-    ## Não gera se estiver na direita
         direita = []
-        direita_values = []
         for i in range(len(map)):
-            #direita
             if (i + 1) % N == 0 :
                 direita.append(i)
-                direita_values.append(estado_atual[i])
-        if posicao not in direita and 0 < estado_atual[posicao+1]:
-            # peça de baixo desce
+        if posicao not in direita and 0 < estado_atual[posicao + 1]:      
             sucessor = list(estado_atual)
             sucessor[posicao] = -2
             sucessor[posicao + 1] = 0
-            return (tuple(sucessor), "➡️",posicao,estado_atual[(posicao + 1)])
+            return (tuple(sucessor), "➡️",estado_atual[(posicao + 1)])
 
-    # Heurística 2: Distância para o resultado espero
-    # Heurística adminissível, pois, sempre o resultado chega mais perto
-    # Transformei o array em matriz para fazer cálculo de distância
-    def heuristica2(self, estado):
+    # def _cima_direita(self, posicao, estado_atual):
+    #     direita = []
+    #     for i in range(len(map)):
+    #         if (i + 1) % N == 0 :
+    #             direita.append(i)
+    #     cima = []
+    #     for i in range(len(map)):
+    #         if 0 <= i < N:
+    #             cima.append(i)
+    #     if (posicao not in cima and posicao not in direita) and 0 < estado_atual[posicao - (N - 1)] :
+    #         sucessor = list(estado_atual)
+    #         sucessor[posicao] = -2
+    #         sucessor[posicao - (N - 1)] = 0
+    #         return (tuple(sucessor), "Cima-direita",estado_atual[posicao - (N - 1)])
+    # def _cima_esquerda(self, posicao, estado_atual):
+    #     cima = []
+    #     for i in range(len(map)):
+    #         if 0 <= i < N:
+    #             cima.append(i)
+    #     esquerda = []
+    #     for i in range(len(map)):
+    #         if i == 0 or i % N == 0:
+    #             esquerda.append(i)
+    #     if (posicao not in cima and posicao not in esquerda) and 0 < estado_atual[posicao - (N + 1)]:
+    #         sucessor = list(estado_atual)
+    #         sucessor[posicao] = -2
+    #         sucessor[posicao - (N + 1)] = 0
+    #         return (tuple(sucessor), "Cima-Esquerda",estado_atual[posicao - (N + 1)])
+
+    def manhattan(self, estado):
         map_matrix = np.reshape(estado, (M, N))
         x = 0
         y = 0
@@ -109,44 +107,59 @@ class Jogo:
                 if map_matrix[i][j] == 0:
                     x = i
                     y = j
+                    return self.distancia_manhattan(x,y)
 
-        return self.distancia_manhattan(x,y)
     # Distância de Manhattan: d = |xi-xj| + |yi-yj|
     #                             |xi-M-1| + |yi-N-1|
     def distancia_manhattan(self,i, j):
         return abs(i-(M - 1))+abs(j-(N - 1))
-        
-    # Função de custo: Quando custa mover de um 
-    # estado_origem para estado_destino. No Quebra Cabeça 
-    # de 8, este custo é fixo e arbitrariamente será 1.
+    
+    # def euclidiana(self,estado):
+    #     map_matrix = np.reshape(estado, (M, N))
+    #     x = 0
+    #     y = 0
+    #     for i in range(len(map_matrix)):
+    #         for j in range(len(map_matrix[i])):
+    #             if map_matrix[i][j] == 0:
+    #                 x = i
+    #                 y = j
+    #     return self.distancia_euclidiana(x,y)
+
+    # def distancia_euclidiana(self,i, j):
+    #     return sqrt((i - (M-1))**2 + (j - (N - 1)**2))
+
     def custo(self, estado_origem, estado_destino):
         return estado_origem + estado_destino
-
-
 
 if __name__ == "__main__":
     g = Jogo()
     terrains = {'T':1,'A': 3,'Am': 6,'B':-1}
-    M = int(input("Digite o valor para M: "))
-    N = int(input("Digite o valor para N: "))
+    M = int(input("Digite o valor para matriz quadrada: "))
+    N = M
     array_size = M * N
     map = create_map(array_size,terrains)
     map_values = tuple(create_map_values(array_size,terrains,map))
-    mapa = tuple([0,3,3,-1,-1,1,6,6,-1,-1,3,6, 6,1,1,3])
     map_matrix = np.reshape(map, (M, N))
-    map_matrixValues = np.reshape(mapa, (M, N))
+    map_matrixValues = np.reshape(map_values, (M, N))
     print(map_matrix)
-    print(map_matrixValues)
-    no_solucao = a_estrela(mapa, 
+    Manhattan = a_estrela(map_values, 
                             g.testar_objetivo, 
                             g.gerar_sucessores, 
-                            g.heuristica2,
-                            g.custo)
-    print(no_solucao)
+                            g.manhattan,
+                            g.custo,
+                            M,
+                            N)
 
-    if(no_solucao is None):
+    # euclidiana = a_estrela(map_values, 
+    #                         g.testar_objetivo, 
+    #                         g.gerar_sucessores, 
+    #                         g.euclidiana,
+    #                         g.custo)
+
+    if(Manhattan is None):
         print("Não houve solução ao problema")
     else:
         print("Solução:")
-        caminho = vertice_caminho(no_solucao)
-        print(caminho)
+        # print(vertice_caminho(Manhattan))
+        print("Custo total dos terrenos: " + str(Manhattan[0]))
+        print("Vértices: " + str(Manhattan[1]))
